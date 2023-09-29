1256450963c371318516729a13ea0e23adb9d60b
unknown <ranasingh061177@gmail.com>
2023-09-29 20:06:54 +0530

    created footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
         <div>
            <Navbar/>
            <Hero/>
+           <Footer/>
+                  
         </div>
     );
 }
--- src/constants/url.js
+++ src/constants/url.js
@@ -1,2 +1,3 @@
 export const  Logo = "https://res.cloudinary.com/do4vytmxm/image/upload/v1695803892/images/Logo_ev7w4u.png";
 export const HeroImg="https://res.cloudinary.com/do4vytmxm/image/upload/v1695882108/images/Hero-img1_thoqzj.png"
+export const footerImg = "https://res.cloudinary.com/do4vytmxm/image/upload/v1695975824/Copy_of_HyggeX_3_2_pduhry.png"
