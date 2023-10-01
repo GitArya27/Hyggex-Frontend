0f905fe2cbe97c40a5d4b127ed75c7c2e4193718
aniketpaste1996 <85695209+aniketpaste1996@users.noreply.github.com>
2023-10-01 18:53:25 +0530

    Code Fix
    Code Fix

--- src/components/FAQPage/FAQ.jsx
+++ src/components/FAQPage/FAQ.jsx
@@ -11,7 +11,6 @@ function FAQ ({faq, index, toggleFAQ}) {
 				{faq.question}
 			</div>
 			<div className="faq-answer p-1">
-				<div className="p-2 rounded-3x1 text-[0.72rem] text-[#06286E]">{faq.answer}</div>
 			<div className="p-2 rounded-3x1 text-[0.72rem] text-[#06286E]">{faq.question1}</div>
 				<div className="p-2 rounded-3x1 text-[0.72rem] text-[#696671]">{faq.answer}</div>
 			</div>
