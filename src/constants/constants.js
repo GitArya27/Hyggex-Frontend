5c5266b449a748e96064c21a7691501afd05db6c
unknown <ranasingh061177@gmail.com>
2023-10-04 17:39:21 +0530

    image-change

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -37,7 +37,7 @@ export const whyHyggex = [
     title: "Adaptmate",
     text: "This is a cutting-edge adaptive learning system that recognizes and aligns with a student's unique learning style. By understanding individual strengths and areas of improvement, AdaptMate delivers personalized content, making learning more effective and enjoyable.",
     imageUrl:
-      "https://res.cloudinary.com/do4vytmxm/image/upload/v1696058970/Young_people_working_in_a_team_ybrtas.svg",
+      "https://res.cloudinary.com/do4vytmxm/image/upload/v1696419454/12_1_ipcoco.png",
     position: "left",
   },
   {
@@ -45,7 +45,7 @@ export const whyHyggex = [
     title: "LifeMentor",
     text: "Addressing the critical aspect of well-being, LifeMentor offers individualized mentorship. This component assists students in navigating academic challenges and life's hurdles, ensuring they have the support and guidance essential for personal and academic growth.",
     imageUrl:
-      "https://res.cloudinary.com/do4vytmxm/image/upload/v1696059177/Dashboard_yfmqdz.svg",
+      "https://res.cloudinary.com/do4vytmxm/image/upload/v1696419453/13_1_neybzs.png",
     position: "right",
   },
   {
@@ -53,7 +53,7 @@ export const whyHyggex = [
     title: "TaskTick",
     text: "An intuitive tool designed to boost productivity. By helping students organize, prioritize, and manage their academic and personal tasks, TaskTick instills essential life skills such as time management, goal setting, and consistency.",
     imageUrl:
-      "https://res.cloudinary.com/do4vytmxm/image/upload/v1696060299/Secure_protection_with_fingerprint_and_lock_dbhyxs.svg",
+      "https://res.cloudinary.com/do4vytmxm/image/upload/v1696419454/9_1_cxvhdh.png",
     position: "left",
   },
 ];
