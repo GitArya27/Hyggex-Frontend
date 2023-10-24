b71e15e7ed8edbac182b758edb858cbe2d3b302c
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2023-10-24 22:39:10 +0100

    add

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import Home from "../src/components/LandingPage/Home";
 import Layout from "../src/layout/Layout";
 import Login from "../src/components/auth/Login";
 import React from "react";
-import SignUp from "./components/signUp/signUp";
+import SignUp from "../src/components/signUp/signUp";
 
 export default function App() {
     return (
         <Routes>
             <Route path="/" element={<Layout/>}>
                 <Route index element={<Home/>} />
-                <Route path="/signUp" element={<SignUp/>} />
+
             </Route>
             <Route path="/login" element={<Login />} />
+            <Route path="/signUp" element={<SignUp/>} />
         </Routes>
     );
 }
