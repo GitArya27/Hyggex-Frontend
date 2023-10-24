2170bbb77bb47cc88840744d0f25d509928d2b61
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2023-10-24 22:27:33 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+
+import Home from "../src/components/LandingPage/Home";
 import Layout from "../src/layout/Layout";
 import Login from "../src/components/auth/Login";
-import Home from "../src/components/LandingPage/Home";
+import React from "react";
+import SignUp from "./components/signUp/signUp";
 
 export default function App() {
     return (
         <Routes>
             <Route path="/" element={<Layout/>}>
                 <Route index element={<Home/>} />
+                <Route path="/signUp" element={<SignUp/>} />
             </Route>
-            <Route path="/login" element={<Login/>} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     );
 }
--- src/components/signUp/signUp.jsx
+++ src/components/signUp/signUp.jsx
@@ -1,8 +1,60 @@
+import { Link } from 'react-router-dom'
+import { Logo } from '../../constants/url'
 import React from 'react'
 
 const SignUp = () => {
+    const login1 = "https://res.cloudinary.com/dbnxbly1r/image/upload/v1698181188/login_1_mjpshn.svg";
+    const login2 = "https://res.cloudinary.com/dbnxbly1r/image/upload/v1698181349/login_2_zmmwrs.svg";
+    const login3 = "https://res.cloudinary.com/dbnxbly1r/image/upload/v1698181445/login_3_y0ib97.svg";
+    const logo = "https://res.cloudinary.com/dbnxbly1r/image/upload/v1698181752/Component_9_dqkwgp.png";
+
+
     return (
-        <div>
+        <div className='lg:flex-row justify-between'>
+            <div>
+                <img src={logo} alt="" />
+                <div>
+                    <img src={login1} alt="" />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, beatae.</p>
+                    <button></button>
+                </div>
+            </div>
+
+            <div className='flex flex-column text-center'>
+                <div>
+                    <h1>Sign Up</h1>
+                    <p>Enter profile details</p>
+                </div>
+                <div>
+                    <span>Enter number</span>
+                    <span>Enter basic details</span>
+                </div>
+                <p>We will need your profile details to give you a better experience</p>
+                <form action="">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" placeholder="Sam christy" />
+
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" placeholder="Samchristy98879@gmail.com" />
+
+                    <div>
+                        <label htmlFor="location">Location</label>
+                        <select name="location" id="">
+                            <option value="india">India</option>
+                            <option value="usa">USA</option>
+                            <option value="india">Afghanistan</option>
+                        </select>
+                        <label htmlFor="grade">Grade</label>
+                        <select name="grade" id="">
+                            <option value="india">India</option>
+                            <option value="usa">USA</option>
+                            <option value="india">Afghanistan</option>
+                        </select>
+                    </div>
+                    <button type='submit'>Continue</button><br />
+                    <span>Already have an account <Link>login</Link></span>
+                </form>
+            </div>
 
         </div>
     )
