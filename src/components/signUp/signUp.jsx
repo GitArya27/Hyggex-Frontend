4bb2fdc9b2f131aff7f20b4523845874f6fe8007
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2023-10-25 21:24:28 +0100

    committed

--- src/components/signUp/signUp.jsx
+++ src/components/signUp/signUp.jsx
@@ -31,23 +31,24 @@ const SignUp = () => {
 
     return (
       <div className='flex flex-col justify-center sm:flex-row' id='container'>
-        <div className="bg-[#DFEAFF] sm:w-1/2 bg-cover bg-center relative pb-8" id='card1'>
+        <div className="bg-[#DFEAFF] sm:w-1/2 bg-cover bg-center relative pb-10" id='card1'>
           <img src={logo1} alt="logo" className="ml-8 mb-6 w-32 py-8" id='logo' />
-          <div id='inner-card'>
+          <div id='inner-card' className=''>
 
             <img src={images[activeIndex]} alt="carousel" className="w-60 h-60 mx-auto" id='slider' />
 
-            <div className="absolute bottom-40 left-1/2 transform -translate-x-1/2 flex space-x-2 mt-2">
+            <div className="absolute bottom-10 sm:bottom-40 left-1/2 transform -translate-x-1/2 flex space-x-2 mt-2">
               {images.map((_, index) => (
                 <button
                   key={index}
-                  className={`w-8 h-1 bg-gray-300' ${activeIndex === index ? 'bg-blue-500' : ''}`}
+                  className={`w-6 h-1 ${activeIndex === index ? 'bg-blue-500' : 'bg-white'}`}
                   onClick={() => setActiveIndex(index)}
                 ></button>
 
               ))}
+
             </div>
-            <div className="text-center text-gray-700 mt-2">
+            <div className="text-center text-gray-700 mt-2 mb-10">
               <p id='imagetxt' className="text-xs px-8 leading-5">{imageText[activeTextIndex]}</p>
             </div>
           </div>
@@ -64,29 +65,43 @@ const SignUp = () => {
             <span className='text-xs mx-4'>Enter basic details</span>
           </div>
 
-          <p className="mt-4 md:mt-6 text-gray-600 text-xs text-center" id='profile-details'>We will need your profile details to give you a better experience</p>
+          {/*<p className="mt-4 md:mt-6 text-gray-600 text-xs text-center" id='profile-details'>We will need your profile details to give you a better experience</p>*/}
           <form action="" id='form' className="text-xs text-gray-600 max-w-screen-sm px-20 py-5">
-            <label htmlFor="name" className="text-xs text-gray-600">Name <small>*</small></label>
-            <input type="text" name="name" placeholder="Sam Christy" className="w-full py-2 px-3 border rounded-md mb-6 text-xs" />
+            <label htmlFor="email" className="text-xs text-gray-600">Email Address <small>*</small></label>
+            <input type="email" name="email" placeholder="Samchristy98879@gmail.com" className="w-full py-2 px-3 border rounded-md mb-6 text-xs" />
 
-            <label htmlFor="email" className="text-xs text-gray-600">Email<small>*</small></label>
-            <input type="email" name="email" placeholder="samchristy98879@gmail.com" className="w-full py-2 px-3 border rounded-md mb-6 text-xs" />
+            <label htmlFor="shool" className="text-xs text-gray-600">Are you in school? <small>*</small></label>
+            <select name="location" id="select1" className="w-full py-2 px-3 border rounded-md mb-6 text-xs">
+              <option value=""></option>
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </select>
+
+            <label htmlFor="exam" className="text-xs text-gray-600">Are you preparing for competitive exams? <small>*</small></label>
+            <select name="location" id="select1" className="w-full py-2 px-3 border rounded-md mb-6 text-xs">
+              <option value=""></option>
+              <option value="yes">Yes</option>
+              <option value="CTA">CTA</option>
+              <option value="CLAT">CLAT</option>
+            </select>
 
             <div id="select-container" className="flex items-center justify-between">
               <div>
                 <label htmlFor="location" className="text-xs text-gray-600">Location<small className="text-red-500">*</small></label>
                 <select name="location" id="select1" className="w-full py-2 px-3 border rounded-md">
-                  <option value="india">India</option>
-                  <option value="usa">USA</option>
-                  <option value="afghanistan">Afghanistan</option>
+                  <option value=""></option>
+                  <option value="Chennai">Chennai</option>
+                  <option value="Mubi">Mubi</option>
+                  <option value="Delhi">Delhi</option>
                 </select>
               </div>
               <div>
                 <label htmlFor="grade" className="text-xs text-gray-600">Grade<small>*</small></label>
                 <select name="grade" id="select2" className="w-full py-2 px-3 text-xs border mx-3 rounded-md">
-                  <option value="india">India</option>
-                  <option value="usa">USA</option>
-                  <option value="afghanistan">Afghanistan</option>
+                  <option value=""></option>
+                  <option value="Grade1">Grade1</option>
+                  <option value="Grade2">Grade2</option>
+                  <option value="Grade3">Grade3</option>
                 </select>
               </div>
             </div>
@@ -97,7 +112,7 @@ const SignUp = () => {
           </form>
 
           <span id="link-span" className="text-gray-600 text-center mx-auto text-xs">
-            Already have an account? <Link id="link-to-register" to="/login" className="text-center mx-auto text-xs text-blue-500 hover:underline">Login</Link>
+            Already have an account? <Link id="link-to-register" to="/login" className="text-center no-underline mx-auto text-xs text-blue-500">Login</Link>
           </span><br />
         </div>
 
