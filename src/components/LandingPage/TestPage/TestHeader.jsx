af8e5d817ffb457474716daeb8318b3b7dc912f1
GitArya27 <singharya2792@gmail.com>
2023-12-28 18:08:17 +0530

    Code optimization

---
+++ src/components/LandingPage/TestPage/TestHeader.jsx
@@ -0,0 +1,50 @@
+import "font-awesome/css/font-awesome.min.css";
+import "./TestPage.css";
+import { checklist } from "../../../constants/url";
+import { idea } from "../../../constants/url";
+import { book } from "../../../constants/url";
+import React from "react";
+
+
+const TestHeader = () => {
+  return (
+    <>
+      <div className="head">
+        <h1 className="h1">Free Reading Assessment</h1>
+        <p>HyggeX Assessment Explorer &reg;</p>
+      </div>
+
+      <div className="box1">
+        <img src={checklist} alt="" />
+        <div className="one">
+          <h6>Complete the Assessment</h6>
+          <p>
+            Engage sincerely and respond accurately to unveil your reading
+            profile.
+          </p>
+        </div>
+      </div>
+      <div className="box2">
+        <img src={idea} alt="" />
+        <div className="one">
+          <h6>View Detailed Insights</h6>
+          <p>
+            Explore the impact of comprehension, speed, retention, anxiety,
+            focus, and fatigue on your reading.
+          </p>
+        </div>
+      </div>
+      <div className="box3">
+        <img src={book} alt="" />
+        <div className="one">
+          <h6>Unlock Your Reading Potential</h6>
+          <p>
+            Advance into the proficient reader you aim to be with your optional
+            Premium Suite.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+export default TestHeader;
